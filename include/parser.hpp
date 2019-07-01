9acfa1c82148783f7be2051eacf83e5851aa6544
jwqe764241 <jwqe764241@gmail.com>
2019-07-01 23:23:16 +0900

    add parser to find option

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <map>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <initializer_list>
+
+#include "option.hpp"
+
 class parser
 {
 private:
-	void parse(int count, char** options);
+	//store parsed options
+	std::map<option, std::vector<std::string>> option_map;
+
+private:
+	void parse(int argc, char** argv, std::vector<option> options);
+	auto find(std::string name);
+
 public:
-	parser(int argc, char** argv);
+	parser(int argc, char** argv, std::initializer_list<option> options);
+
+	bool has_option(std::string name);
+	std::vector<std::string> get_arguments(std::string name);
 };
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,41 @@
 #include "parser.hpp"
+
+parser::parser(int argc, char** argv, std::initializer_list<option> options)
+{
+	parse(argc, argv, options);
+}
+
+void parser::parse(int argc, char** argv, std::vector<option> options)
+{
+
+}
+
+auto parser::find(std::string name)
+{
+	auto it = std::find_if(option_map.begin(), option_map.end(), [name](const option& o) {
+		return o.get_name() == name || o.get_short_name() == name;
+	});
+
+	return it;
+}
+
+bool parser::has_option(std::string name)
+{
+	auto it = find(name);
+
+	return it != option_map.end() ? true : false;
+}
+
+std::vector<std::string> parser::get_arguments(std::string name)
+{
+	auto it = find(name);
+
+	if (it != option_map.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		return {};
+	}
+}
