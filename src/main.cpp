f7ed8c02305dd55277c658f64265fbc3dfd770f9
jwqe764241 <jwqe764241@gmail.com>
2019-07-04 18:25:10 +0900

    add test code in main
    test line parameter is
    -a 1 2 3 -b 4 5 -c 6 7 8 9 -d 10

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,38 @@
 
 int main(int argc, char * argv[])
 {
+	parser parser(argc, argv, {
+		line_option("-", "a", 3),
+		line_option("-", "b", 2),
+		line_option("-", "c", 4),
+		line_option("-", "d", 1)
+	});
 
+	std::vector<std::string> a = parser.get_arguments("-a");
+	std::cout << "a options" << std::endl;
+	for (std::string s : a)
+	{
+		std::cout << s << std::endl;
+	}
 
-    std::cout << "Hello World!\n";
+	std::vector<std::string> b = parser.get_arguments("-b");
+	std::cout << "b options" << std::endl;
+	for (std::string s : b)
+	{
+		std::cout << s << std::endl;
+	}
+
+	std::vector<std::string> c = parser.get_arguments("-c");
+	std::cout << "c options" << std::endl;
+	for (std::string s : c)
+	{
+		std::cout << s << std::endl;
+	}
+
+	std::vector<std::string> d = parser.get_arguments("-d");
+	std::cout << "d options" << std::endl;
+	for (std::string s : d)
+	{
+		std::cout << s << std::endl;
+	}
 }
