996f21b00d6b499704f1a7eed9198c863e876162
jwqe764241 <jwqe764241@gmail.com>
2019-11-26 23:05:44 +0900

    Add test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,2 +1,34 @@
 #define CATCH_CONFIG_MAIN
+
 #include "catch.hpp"
+#include "get_option.hpp"
+
+TEST_CASE("Parse test 1", "[parser]")
+{
+	using namespace cmd;
+	
+	int length = 9;
+	char* arguments[9] = {
+		"name",
+		"-a",
+		"1",
+		"-b",
+		"2",
+		"-c",
+		"3",
+		"-d",
+		"4"
+	};
+	
+	parser parser(length, arguments, {
+		option("-a", 1),
+		option("-b", 1),
+		option("-c", 1),
+		option("-d", 1)
+	});
+	
+	REQUIRE(parser.get_arguments("-a")[0] == "1");
+	REQUIRE(parser.get_arguments("-b")[0] == "2");
+	REQUIRE(parser.get_arguments("-c")[0] == "3");
+	REQUIRE(parser.get_arguments("-d")[0] == "4");
+}
